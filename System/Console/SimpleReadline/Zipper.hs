e75ad8255cb722832be399c4d3c2f054ecf67ae8
Vincent Hanquez <vincent@snarc.org>
2012-10-21 18:28:20 +0100

    tweak zipper structure to keep size of prev/next stack.
    also add some extras methods to implement more callbacks.

--- System/Console/SimpleReadline/Zipper.hs
+++ System/Console/SimpleReadline/Zipper.hs
@@ -10,38 +10,56 @@
 module System.Console.SimpleReadline.Zipper
     where
 
-data Zipper a = Zipper [a] [a]
+data Zipper a = Zipper Int [a] Int [a]
 
 zipPrev :: Zipper a -> Zipper a
-zipPrev z@(Zipper p n)
+zipPrev z@(Zipper plen p nlen n)
     | null p    = z
-    | otherwise = Zipper (tail p) (head p : n)
+    | otherwise = Zipper (plen-1) (tail p) (nlen+1) (head p : n)
 
 zipNext :: Zipper a -> Zipper a
-zipNext z@(Zipper p n)
+zipNext z@(Zipper plen p nlen n)
     | null n    = z
-    | otherwise = Zipper (head n : p) (tail n)
+    | otherwise = Zipper (plen+1) (head n : p) (nlen-1) (tail n)
 
 zipAtHome :: Zipper a -> Zipper a
-zipAtHome (Zipper p n) = Zipper [] (reverse p ++ n)
+zipAtHome (Zipper plen p nlen n) = Zipper 0 [] (nlen + plen) (reverse p ++ n)
 
 zipAtEnd :: Zipper a -> Zipper a
-zipAtEnd (Zipper p n) = Zipper (reverse n ++ p) []
+zipAtEnd (Zipper plen p nlen n) = Zipper (plen + nlen) (reverse n ++ p) 0 []
 
 zipInsert :: [a] -> Zipper a -> Zipper a
-zipInsert l (Zipper p n) = Zipper (reverse l ++ p) n
+zipInsert l (Zipper plen p nlen n) = Zipper (plen + length l) (reverse l ++ p) nlen n
 
 zipInit :: [a] -> Zipper a
-zipInit l = Zipper [] l
+zipInit l = Zipper 0 [] (length l) l
+
+zipDelPrev :: Int -> Zipper a -> Zipper a
+zipDelPrev nb z@(Zipper plen p nlen n)
+    | nb >= plen = Zipper 0 [] nlen n
+    | otherwise  = Zipper (plen - nb) (drop nb p) nlen n
+
+zipDelNext :: Int -> Zipper a -> Zipper a
+zipDelNext nb z@(Zipper plen p nlen n)
+    | nb >= nlen = zipDelToEnd z
+    | otherwise  = Zipper plen p (nlen - nb) (drop nb n)
+
+zipDelToEnd :: Zipper a -> Zipper a
+zipDelToEnd (Zipper plen p _ _) = Zipper plen p 0 []
 
 zipToList :: Zipper a -> [a]
-zipToList (Zipper p n) = reverse p ++ n
+zipToList (Zipper _ p _ n) = reverse p ++ n
 
 zipHasNext :: Zipper a -> Bool
-zipHasNext (Zipper _ n) = not $ null n
+zipHasNext (Zipper _ _ _ n) = not $ null n
 
 zipLengthNext :: Zipper a -> Int
-zipLengthNext (Zipper _ n) = length n
+zipLengthNext (Zipper _ _ nlen _) = nlen
 
 zipLengthPrev :: Zipper a -> Int
-zipLengthPrev (Zipper p _) = length p
+zipLengthPrev (Zipper plen _ _ _) = plen
+
+safeDrop n l
+	| null l    = []
+	| n == 0    = l
+	| otherwise = safeDrop (n-1) (tail l)
